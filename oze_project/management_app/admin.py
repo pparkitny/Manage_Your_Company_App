7a08e90389c0b03ba68d07e7bfcf76bc265f46e6
Patryk Parkitny <pat.parkitny@gmail.com>
2021-12-12 16:35:45 +0100

    Add models and relations

--- oze_project/management_app/admin.py
+++ oze_project/management_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Squad, Employee, Investment, DayName, SquadInvestment
+
+admin.site.register(Squad)
+admin.site.register(Employee)
+admin.site.register(Investment)
+admin.site.register(SquadInvestment)
+admin.site.register(DayName)
+
+
--- oze_project/management_app/models.py
+++ oze_project/management_app/models.py
@@ -1,3 +1,71 @@
 from django.db import models
 
-# Create your models here.
+POSITION = (
+    (1, "Instalator"),
+    (2, "Elektryk"),
+    (3, "Pracownik biurowy"),
+    (4, "Handlowiec")
+)
+
+TYPES_OF_INVESTMENT = (
+    (1, "Panele fotowoltaiczne"),
+    (2, "Pompa ciepła"),
+    (3, "Kolektory słoneczne"),
+    (4, "Kocioł")
+)
+
+
+class Squad(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    position = models.IntegerField(choices=POSITION)
+    squad = models.ForeignKey(Squad, on_delete=models.CASCADE)
+
+    @property
+    def name(self):
+        return "{} {}".format(self.first_name, self.last_name)
+
+    def __str__(self):
+        return self.name
+
+
+class Investment(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    street_name = models.CharField(max_length=128)
+    city_name = models.CharField(max_length=128)
+    zip_code = models.CharField(max_length=10)
+    type_of_investment = models.IntegerField(choices=TYPES_OF_INVESTMENT)
+    workers = models.ManyToManyField(Squad, through='SquadInvestment')
+
+    @property
+    def name(self):
+        return "{}, {}".format(self.street_name, self.city_name)
+
+    def __str__(self):
+        return self.name
+
+
+class SquadInvestment(models.Model):
+    investment = models.ForeignKey(Investment, on_delete=models.CASCADE)
+    squad = models.ForeignKey(Squad, on_delete=models.CASCADE)
+
+    @property
+    def squad_and_investment(self):
+        return "{} - {}".format(self.investment, self.squad)
+
+    def __str__(self):
+        return self.squad_and_investment
+
+class DayName(models.Model):
+    name = models.TextField(null=False, unique=True)
+
+    def __str__(self):
+        return self.name
